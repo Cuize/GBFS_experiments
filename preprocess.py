8474da4b8aa0fbb689dfe8492ef5e1793b42a3ba
Cuize <cuize@9801a7a2255d.ant.amazon.com>
2018-09-20 17:33:02 -0700

    take into customerized starting features

--- preprocess.py
+++ preprocess.py
@@ -1,37 +1,38 @@
 import pandas as pd
 import numpy as np
+from os import listdir
 import sys
-import csv
 def generate_unused(train,test,response_name,folder):
 	train=pd.read_csv(train,sep="\t")
 	train_names=list(train.columns.values)
+	file_names=listdir(folder)
+	if "allfeature.txt" in file_names:
+		allfeature=set(pd.read_csv(folder+"/allfeature.txt",header=None)[0])
+	else:
+		allfeature=None
 	unused=[]
 	for name in train_names:
 		if name==response_name:
 			continue
+		elif allfeature and name not in allfeature:
+			unused.append(name)
 		else:
-			unique=train[name].unique()
-			dtype=unique[0]
-			try:
-				dtype.isalnum()
+			if type(train[name][0])==str:
 				unused.append(name)
-			except:
+			else:
+				unique=train[name].unique()
 				if len(unique)==1:
 					unused.append(name)
-
 	txtfile1=folder+"/preprocess_unused.txt"
 	txtfile2=folder+"/trueY.txt"
 	txtfile3=folder+"/keyword.txt"
+	pd.DataFrame(unused).to_csv(txtfile1,header=None,index=None,sep="/")
 	test=pd.read_csv(test,sep="\t")
 	test[response_name].to_csv(txtfile2, index=False)
 	try:
 		test["keyword"].to_csv(txtfile3, index=False)
 	except:
 		print("not query-asin-like dataset,no keywords given")
-	with open(txtfile1, "w") as output:
-		writer = csv.writer(output, lineterminator='\n',escapechar=' ',quoting=csv.QUOTE_NONE)
-		for val in unused:
-			writer.writerow([val])
 
 
 if __name__ == '__main__':
