9d84d2bc278f402382c0f3b055f26b29100e7dc6
Cuize <cuize@9801a7a2255d.ant.amazon.com>
2018-10-15 10:35:29 -0700

    add GBDT_tuned.txt option

--- tuned_vary_mu.sh
+++ tuned_vary_mu.sh
@@ -4,7 +4,7 @@
 ## input: tuned_GBFS.txt(1) data_name(2) response_name(3) tuned_GBDT.txt(4)
 ## output: preds.txt boosting_rmse.txt for different tuned parameters
 ## order in tuned_GBFS.txt: mu shrink alpha iterN
-## order in tuned_GBDT.txt: shrink alpha iterN
+## order in tuned_GBDT.txt: shrink alpha iterN topk
 
 #alias
 par=$1
@@ -76,30 +76,6 @@ do
 		echo "$name" "$mu" "$shrink" "$alpha" "$iterN" "$attrn" "$rms" "$SECONDS">> "$output"
 		mv preds.txt "$result_path"/"$name"_preds_mu"$mu"_alpha"$alpha"_shrink"$shrink"_itern"$iterN"_attrn"$attrn"_rms"$rms".txt
 		mv feature_scores.txt "$result_path"/"$name"_feature_scores_mu"$mu"_alpha"$alpha"_shrink"$shrink"_itern"$iterN"_attrn"$attrn"_rms"$rms".txt
-
-
-		# GBDTBTt
-		cat "$par_gbdt"|
-		while read gbdt_pars
-		do
-			shrink_gbdt="$( echo "$gbdt_pars"|cut -f 1 -d" " )"
-			alpha_gbdt="$( echo "$gbdt_pars"|cut -f 2 -d" "  )"
-			iterN_gbdt="$( echo "$gbdt_pars"|cut -f 3 -d" " )"
-			SECONDS=0
-			name=GBDTBTt
-			"$BT" -t "$converted_train_data" -v "$converted_test_data" -r "$all_attr" -a "$alpha_gbdt" -k -1
-			rm preds.txt model.bin bagging_rms.txt "$data_name"_train.fs.attr log.txt
-			mv feature_scores.txt "$result_path"/feature_scores_BT_alpha"$alpha_gbdt".txt
-			rank_all="$result_path"/feature_scores_BT_alpha"$alpha_gbdt".txt
-			python generate_topk.py "$rank_all" "$all_attr" "$attrn" "$data_name"    #output temperary .attr file called "$data_name"_temp.attr
-			"$GBFS" -t "$converted_train_data" -v "$converted_test_data" -r "$data_name"_temp.attr -a "$alpha_gbdt" -sh "$shrink_gbdt" -n "$iterN_gbdt" -k -1
-			attrn="$(head -1 feature_scores.txt| cut -c  26-)"
-			rms="$(tail -1 boosting_rms.txt)"
-			echo "$name" 0 "$shrink_gbdt" "$alpha_gbdt" "$iterN_gbdt" "$attrn" "$rms" "$SECONDS">> "$output"
-			mv preds.txt "$result_path"/"$name"_preds_alpha"$alpha_gbdt"_shrink"$shrink_gbdt"_itern"$iterN_gbdt"_attrn"$attrn"_rms"$rms".txt
-			mv feature_scores.txt "$result_path"/"$name"_feature_scores_alpha"$alpha_gbdt"_shrink"$shrink_gbdt"_itern"$iterN_gbdt"_attrn"$attrn"_rms"$rms".txt
-
-		done
 		SECONDS=0
 		name=GBFS_adapt2_model
 		"$GBFS_adapt2" -t "$converted_train_data" -v "$converted_test_data" -r "$all_attr" -mu "$mu" -sh "$shrink" -a "$alpha" -n "$iterN" -k -1
@@ -110,32 +86,29 @@ do
 	mv preds.txt "$result_path"/"$name"_preds_mu"$mu"_alpha"$alpha"_shrink"$shrink"_itern"$iterN"_attrn"$attrn"_rms"$rms".txt
 	mv feature_scores.txt "$result_path"/"$name"_feature_scores_mu"$mu"_alpha"$alpha"_shrink"$shrink"_itern"$iterN"_attrn"$attrn"_rms"$rms".txt
 
+done
 
-
-	# GBDTBTt
-	cat "$par_gbdt"|
-	while read gbdt_pars
-	do
-		shrink_gbdt="$( echo "$gbdt_pars"|cut -f 1 -d" " )"
-		alpha_gbdt="$( echo "$gbdt_pars"|cut -f 2 -d" "  )"
-		iterN_gbdt="$( echo "$gbdt_pars"|cut -f 3 -d" " )"
-		SECONDS=0
-		name=GBDTBTt
-		"$BT" -t "$converted_train_data" -v "$converted_test_data" -r "$all_attr" -a "$alpha_gbdt" -k -1
-		rm preds.txt model.bin bagging_rms.txt "$data_name"_train.fs.attr log.txt
-		mv feature_scores.txt "$result_path"/feature_scores_BT_alpha"$alpha_gbdt".txt
-		rank_all="$result_path"/feature_scores_BT_alpha"$alpha_gbdt".txt
-		python generate_topk.py "$rank_all" "$all_attr" "$attrn" "$data_name"    #output temperary .attr file called "$data_name"_temp.attr
-		"$GBFS" -t "$converted_train_data" -v "$converted_test_data" -r "$data_name"_temp.attr -a "$alpha_gbdt" -sh "$shrink_gbdt" -n "$iterN_gbdt" -k -1
-		attrn="$(head -1 feature_scores.txt| cut -c  26-)"
-		rms="$(tail -1 boosting_rms.txt)"
-		echo "$name" 0 "$shrink_gbdt" "$alpha_gbdt" "$iterN_gbdt" "$attrn" "$rms" "$SECONDS">> "$output"
-		mv preds.txt "$result_path"/"$name"_preds_alpha"$alpha_gbdt"_shrink"$shrink_gbdt"_itern"$iterN_gbdt"_attrn"$attrn"_rms"$rms".txt
-		mv feature_scores.txt "$result_path"/"$name"_feature_scores_alpha"$alpha_gbdt"_shrink"$shrink_gbdt"_itern"$iterN_gbdt"_attrn"$attrn"_rms"$rms".txt
-
-	done
-
-
+# GBDTBTt
+cat "$par_gbdt"|
+while read gbdt_pars
+do
+	shrink_gbdt="$( echo "$gbdt_pars"|cut -f 1 -d" " )"
+	alpha_gbdt="$( echo "$gbdt_pars"|cut -f 2 -d" "  )"
+	iterN_gbdt="$( echo "$gbdt_pars"|cut -f 3 -d" " )"
+	topk="$( echo "$gbdt_pars"|cut -f 4 -d" " )"
+	SECONDS=0
+	name=GBDTBTt
+	"$BT" -t "$converted_train_data" -v "$converted_test_data" -r "$all_attr" -a "$alpha_gbdt" -k -1
+	rm preds.txt model.bin bagging_rms.txt "$data_name"_train.fs.attr log.txt
+	mv feature_scores.txt "$result_path"/feature_scores_BT_alpha"$alpha_gbdt".txt
+	rank_all="$result_path"/feature_scores_BT_alpha"$alpha_gbdt".txt
+	python generate_topk.py "$rank_all" "$all_attr" "$topk" "$data_name"    #output temperary .attr file called "$data_name"_temp.attr
+	"$GBFS" -t "$converted_train_data" -v "$converted_test_data" -r "$data_name"_temp.attr -a "$alpha_gbdt" -sh "$shrink_gbdt" -n "$iterN_gbdt" -k -1
+	attrn="$(head -1 feature_scores.txt| cut -c  26-)"
+	rms="$(tail -1 boosting_rms.txt)"
+	echo "$name" 0 "$shrink_gbdt" "$alpha_gbdt" "$iterN_gbdt" "$attrn" "$rms" "$SECONDS">> "$output"
+	mv preds.txt "$result_path"/"$name"_preds_alpha"$alpha_gbdt"_shrink"$shrink_gbdt"_itern"$iterN_gbdt"_attrn"$attrn"_rms"$rms".txt
+	mv feature_scores.txt "$result_path"/"$name"_feature_scores_alpha"$alpha_gbdt"_shrink"$shrink_gbdt"_itern"$iterN_gbdt"_attrn"$attrn"_rms"$rms".txt
 done
 rm "$data_name"_temp.attr
 
